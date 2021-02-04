d538ad1fe3fa5f56867ec28370c1a2d29dd7d79c
Heshan <heshansandeepa94@gmail.com>
2021-02-04 13:49:19 +0530

    Second tutorial(@Primary)

--- demo/demo/src/main/java/com/example/sort/QuickSort.java
+++ demo/demo/src/main/java/com/example/sort/QuickSort.java
@@ -1,7 +1,10 @@
 package com.example.sort;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class QuickSort implements SortingAlgo{
 
 	public int[] sort(int[] array) {
